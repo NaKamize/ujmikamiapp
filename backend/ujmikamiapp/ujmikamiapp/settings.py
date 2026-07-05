ca99b5e0f04eabd8cf4857d305d9dc333d5f9b5a
NaKamize <makisjozef28@gmail.com>
2026-07-05 19:14:44 +0200

    update of for azure mysql

--- backend/ujmikamiapp/ujmikamiapp/settings.py
+++ backend/ujmikamiapp/ujmikamiapp/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'ujmikamiapp.wsgi.application'
 DATABASES = {
     'default': env.db(default=f'mysql://ujmikamiapp:ujmikamiapp@127.0.0.1:3306/ujmikamiapp'),
 }
-
+DATABASES['default']['OPTIONS'] = {'ssl': {'ca': '/etc/ssl/certs/ca-certificates.crt'}}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
