7225fb30f186cb30787bb999f3b954065c817925
NaKamize <makisjozef28@gmail.com>
2026-07-05 22:42:58 +0200

    Fixing django deployment

---
+++ backend/ujmikamiapp/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+if [ "$PRODUCTION" != "true" ]; then
+    echo "Running seeders (development mode)..."
+    python manage.py seed_ml_models
+    python manage.py seed_projects
+    python manage.py seed_publications
+    python manage.py seed_experiences
+    python manage.py seed_aboutme
+fi
+
+# Rozhodnutie o serveri
+if [ "$PRODUCTION" = "true" ]; then
+    echo "Starting Gunicorn..."
+    exec gunicorn --bind 0.0.0.0:8000 ujmikamiapp.wsgi:application
+else
+    echo "Starting Development Server..."
+    exec python manage.py runserver 0.0.0.0:8000
+fi
