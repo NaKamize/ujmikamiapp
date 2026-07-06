cdbf3df55b4fb247af56773701f0326f7be6bf2c
NaKamize <makisjozef28@gmail.com>
2026-07-06 16:44:58 +0200

    static root path

--- backend/ujmikamiapp/ujmikamiapp/settings.py
+++ backend/ujmikamiapp/ujmikamiapp/settings.py
@@ -124,6 +124,7 @@ USE_AZURE_STORAGE = env('PRODUCTION', default='false')
 
 if USE_AZURE_STORAGE == 'false':
     STATIC_URL = 'static/'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
 else:
